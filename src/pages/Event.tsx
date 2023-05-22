f7f109868130f141beaa89cfc78b511680450a1f
Danthesage01 <ayenibabajidedaniel@gmail.com>
2023-05-22 20:01:37 +0100

    css fixes for events page

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import image from "../assets/Event.png";
+import iconDate from "../assets/Icon-date.png";
+import iconLocation from "../assets/Icon-location.png";
+import { FaChevronRight } from "react-icons/fa";
 
 const event = () => {
   return (
@@ -8,6 +11,38 @@ const event = () => {
         <div className="event">
           <div className="event-paragraph">
             <h2 className="event-heading">Birthday Bash</h2>
+            <p>Hosted by <span className="event-accent">Elysia</span></p>
+            <div className="date">
+              <img
+                src={iconDate}
+                alt="date"
+              />
+              <div className="event-text">
+                <span className="text-1">18 August 6:00PM</span>
+                <br />
+                to <span className="text-span-1">19 August 1:00PM </span>UTC +
+                10
+              </div>
+              <FaChevronRight
+                color="grey"
+                cursor={"pointer"}
+              />
+            </div>
+            <div className="location">
+              <img
+                src={iconLocation}
+                alt="date"
+              />
+              <div className="event-text-2">
+                <span className="text-2">Street name</span>
+                <br />
+                to <span className="text-span">Suburb, State, Postcode</span>
+              </div>
+              <FaChevronRight
+                color="grey"
+                cursor={"pointer"}
+              />
+            </div>
           </div>
           <div className="event-footer">
             <img
@@ -15,14 +50,14 @@ const event = () => {
               alt="image"
               className="event-image"
             />
-            <Link
-              to="/events"
-              className="btn btn-1"
-            >
-              🎉 Create my event{" "}
-            </Link>
           </div>
         </div>
+        <Link
+          to="/"
+          className="btn btn-3"
+        >
+          Back Home
+        </Link>
       </div>
     </div>
   );
